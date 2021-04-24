70318396d06e0085cd716609c713ded2b1ec9002
Sulushash <201533@astanait.edu.kz>
2021-04-24 19:10:21 +0600

    second commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+     include_once 'header.php';
+?>
+<body>
+      <main>
+      <div class="container-sm">
+           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                   <div class="carousel-item active">
+                       <img class="d-block w-100" src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:1400:600:0/bg:ffffff/q:100/plain/s3://complex-images/29/1954/2100-ru.png@jpeg" alt="First slide">
+                    </div>
+                   <div class="carousel-item">
+                       <img class="d-block w-100" src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:1400:600:0/bg:ffffff/q:100/plain/s3://complex-images/29/1955/2100-ru.png@jpeg" alt="Second slide">
+                    </div>
+                   <div class="carousel-item">
+                       <img class="d-block w-100" src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:1400:600:0/bg:ffffff/q:100/plain/s3://complex-images/29/1953/2100-ru.png@jpeg" alt="Third slide">
+                   </div>
+              </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                 <span class="sr-only">Next</span>
+          </a>
+              </div>
+          </div>
+     </main>
+<?php
+     include_once 'footer.php';
+?>
