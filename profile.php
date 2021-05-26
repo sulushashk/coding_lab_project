4178d2fd9624c59bdbd22268c3b3324d61534c10
Sulushash <201533@astanait.edu.kz>
2021-05-26 12:45:57 +0600

    commit

---
+++ profile.php
@@ -0,0 +1,35 @@
+<?php
+     include_once 'header.php';
+?>    
+
+        <div class="container">    
+                  <div class="row">
+                      <div class="panel panel-default">
+                      <div class="panel-heading">  <h2 >User Profile</h2></div>
+                       <div class="panel-body">
+                      <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
+                       <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" height=300px width=300px class="img-circle img-responsive"> 
+                     
+                 
+                      </div>
+                      <div class="col-md-8 col-xs-12 col-sm-6 col-lg-8" >
+                          <div class="container" >
+                            <h2>Daniel</h2>                        
+                           
+                          </div>
+                           <hr>
+                          <ul class="container details" >
+                            <li><p><span class="glyphicon glyphicon-user one" style="width:50px;"></span>A customer</p></li>
+                            <li><p><span class="glyphicon glyphicon-envelope one" style="width:50px;"></span>dan@gmail.com</p></li>
+                          </ul>
+                          <hr>
+                          <div class="col-sm-5 col-xs-6 tital " >Date Of Joining: 15 May 2021</div>
+                      </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        
+<?php
+     include_once 'footer.php';
+?>
