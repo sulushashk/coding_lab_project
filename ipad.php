44f34f5cafdbde37cbf103bc93cfc68d6e3394af
Nurmukhammed <respect.nur03@gmail.com>
2021-06-13 23:34:42 +0600

    Merge branch 'main' of https://github.com/sulushashk/coding_lab_project

--- ipad.php
+++ ipad.php
@@ -1,150 +1,85 @@
 <?php
-     include_once 'header.php';
+include_once 'header.php';
 ?>
 
+<body>
+    <div class="container-md">
+        <div class="uno">
+            <img src="images/ipad.png">
+        </div>
+    </div>
 
-        <div class="container-md">
-    <div class="uno">
-        <img src="images/ipad.png">
-        </div></div>
-    
-        <div class="container" >
-             <div id="wrapper">
-           <ul class="nospace group">
-               <div class="section">
-                  <li class="one_third first">
-                     <article class="element">
-                        <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100956100874/201109150017451116.jpg@jpeg" alt="">
-                           <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                        </figure>
-                <div class="excerpt">
-                     <p>iPad Air 4, 64GB, "Pacific Blue" . Apple  Inc Technology</p>
-                 <footer><a href="pay.html">Buy for 1380,54$ &raquo;</a></footer>
-                 </div>
-                      </article>
-                   </li>
-                  </div>
-                <div class="section">   
-                    <li class="one_third">
-                       <article class="element">
-                           <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100956539533/201109150018708925.jpg@jpeg" alt="">
-                               <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                          </figure>
-                <div class="excerpt">
-                       <p>iPad Air 4, 64GB, Graphite . Apple  Inc Technology</p>
-                 <footer><a href="pay.html">Buy for 1380,54$ &raquo;</a></footer>
-                  </div>
-                       </article>
-                   </li>
-               </div>
-                <div class="section">
-            <li class="one_third">
-                 <article class="element">
-                    <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100956978703/201109150018307119.jpg@jpeg" alt="">
-                         <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                    </figure>
-              <div class="excerpt">
-                       <p>iPad Air 4, 64GB, Gold . Apple  Inc Technology</p>
-                        <footer><a href="pay.html">Buy for 1380,54$ &raquo;</a></footer>
-                    </div>
-                 </article>
-                </li>
-              </div>
-            </ul>
-      </div>
-      </div>
-        
-        <div class="container">
-      <div id="wrapper">
-           <ul class="nospace group">
-               <div class="section">
-                  <li class="one_third first">
-                     <article class="element">
-                        <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100957855533/201109150019092569.jpg@jpeg" alt="">
-                           <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                        </figure>
-                <div class="excerpt">
-                     <p>iPad Air 4, 64GB, "Pacific Blue" . Apple  Inc Technology</p>
-                 <footer><a href="pay.html">Buy for 1530,54$ &raquo;</a></footer>
-                 </div>
-                      </article>
-                   </li>
-                  </div>
-                <div class="section">   
-                    <li class="one_third">
-                       <article class="element">
-                           <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100957416391/201109150017867753.jpg@jpeg" alt="">
-                               <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                          </figure>
-                <div class="excerpt">
-                       <p>iPad Air 4, 64GB, 256 GB, Silver . Apple  Inc Technology</p>
-                 <footer><a href="pay.html">Buy for 1530,54$ &raquo;</a></footer>
-                  </div>
-                       </article>
-                   </li>
-               </div>
-                <div class="section">
-            <li class="one_third">
-                 <article class="element">
-                    <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916101002295388/201109150017002639.jpg@jpeg" alt="">
-                         <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                    </figure>
-              <div class="excerpt">
-                       <p>iPad Air 4, 64GB, Gold . Apple  Inc Technology</p>
-                        <footer><a href="pay.html">Buy for 1530,54$ &raquo;</a></footer>
-                    </div>
-                 </article>
-                </li>
-              </div>
-            </ul>
-      </div>
-      </div>
-        <div class="container">
-      <div id="wrapper">
-           <ul class="nospace group">
-               <div class="section">
-                  <li class="one_third first">
-                     <article class="element">
-                        <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100957416391/201109150017867753.jpg@jpeg" alt="">
-                           <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                        </figure>
-                <div class="excerpt">
-                     <p>iPad Air 4, 64GB, Silver . Apple  Inc Technology</p>
-                 <footer><a href="pay.html">Buy for 1716,45$ &raquo;</a></footer>
-                 </div>
-                      </article>
-                   </li>
-                  </div>
-                <div class="section">   
-                    <li class="one_third">
-                       <article class="element">
-                           <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100959170756/201109150018307119.jpg@jpeg" alt="">
-                               <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                          </figure>
-                <div class="excerpt">
-                       <p>iPad Air 4, 64GB, 512 GB, Gold . Apple  Inc Technology</p>
-                 <footer><a href="pay.html">Buy for 1716,45$ &raquo;</a></footer>
-                  </div>
-                       </article>
-                   </li>
-               </div>
-                <div class="section">
-            <li class="one_third">
-                 <article class="element">
-                    <figure><img src="https://cdn0.ipoint.kz/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fit:230/plain/s3://catalog-products/200916100958732824/201109150018708925.jpg@jpeg" alt="">
-                         <figcaption><a href="pay.html"><i class="fa fa-eye"></i></a></figcaption>
-                    </figure>
-              <div class="excerpt">
-                       <p>iPad Air 4, 64GB, 512 GB, Graphite . Apple  Inc Technology</p>
-                        <footer><a href="pay.html">Buy for 1716,45$ &raquo;</a></footer>
-                    </div>
-                 </article>
-                </li>
-              </div>
-            </ul>
-      </div>
-      </div>
+    <?php
+  require_once 'includes/db.inc.php';
+  ?>
+  <?php
+  if (isset($_POST['add_to_card'])) {
+    if (isset($_SESSION['category_id'])) {
+      $session_array_id = array_column($_SESSION['category_id'], "id");
 
-<?php
-     include_once 'footer.php';
-?>
+      if (!in_array($_GET['id'], $session_array_id)) {
+        $session_array = array(
+          'id' => $_GET['id'],
+          "category_id" => $_POST['category_id'],
+          "name" => $_POST['name'],
+          "price" => $_POST['price'],
+          "status" => $_POST['status']
+        );
+        $_SESSION['category_id'][] = $session_array;
+      }
+    } else {
+
+      $session_array = array(
+        'id' => $_GET['id'],
+        "name" => $_POST['name'],
+        "description" => $_POST['description'],
+        "price" => $_POST['price'],
+        "status" => $_POST['status']
+      );
+      $_SESSION['category_id'][] = $session_array;
+    }
+  }
+  ?>
+  <div class="container">
+  <div class="position-relative">
+  <div class="position-absolute me-5"></div>
+
+        <?php
+
+        $sql = "SELECT * FROM products WHERE category_id=2;";
+        $result = mysqli_query($conn, $sql);
+
+        while ($row = mysqli_fetch_array($result)) { ?>
+
+          
+            <form method="post" action="products.php?id=<?= $row['id'] ?>">
+              <div class="col-md-4">
+                <div class="section">
+                <li class="one_third">
+                  <article class="element">
+                    <figure>
+                      <img src="images/<?= $row['image'] ?>" style='height: 200px;'>
+                      <figcaption><a href="pay.php"><i class="fa fa-eye"></i></a></figcaption>
+                    </figure>
+                    <p><?= $row['name']; ?></p>
+                    <p><?= $row['description']; ?></p>
+                    <footer><a href="pay.php"><?= number_format($row['price'], 2); ?></p></a></footer>
+                    <input type="hidden" name="name" value="<?= $row['name'] ?>">
+                    <input type="hidden" name="price" value="<?= $row['price'] ?>">
+                    <input type="number" name="status" value="1" class="form-control">
+                    <input type="submit" name="add_to_card" class="btn btn-primary my-2" value="Choose">
+                  </article>
+                </li>
+              </div></div>
+            </form>
+          
+
+        <?php
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
+    <?php
+    include_once 'footer.php';
+    ?>
