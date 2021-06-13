9ab8a5e18067f3f05f4ead2bc25ecd1c54defb69
Nurmukhammed <respect.nur03@gmail.com>
2021-06-13 23:26:01 +0600

    cofgf

--- airpods.php
+++ airpods.php
@@ -1,13 +1,18 @@
 <?php
 include_once 'header.php';
 ?>
+<<<<<<< HEAD
 <div class="additijon"></diV>
+=======
+
+>>>>>>> adc8c2e331a27ad6c52dacab175d6e23831e9669
 <div class="container-md">
     <div class="air">
         <img src="https://ubuykw.s3.amazonaws.com/ubuy/wysiwyg/1554270943_AirPods_Accessories_Banner.png">
     </div>
 </div>
 
+<<<<<<< HEAD
 <div class="container">
     <div id="wrapper">
         <ul class="nospace group">
@@ -144,6 +149,81 @@ include_once 'header.php';
         </ul>
     </div>
 </div>
+=======
+
+<?php
+  require_once 'includes/db.inc.php';
+  ?>
+  <?php
+  if (isset($_POST['add_to_card'])) {
+    if (isset($_SESSION['category_id'])) {
+      $session_array_id = array_column($_SESSION['category_id'], "id");
+
+      if (!in_array($_GET['id'], $session_array_id)) {
+        $session_array = array(
+          'id' => $_GET['id'],
+          "category_id" => $_POST['category_id'],
+          "name" => $_POST['name'],
+          "price" => $_POST['price'],
+          "status" => $_POST['status']
+        );
+        $_SESSION['category_id'][] = $session_array;
+      }
+    } else {
+
+      $session_array = array(
+        'id' => $_GET['id'],
+        "name" => $_POST['name'],
+        "description" => $_POST['description'],
+        "price" => $_POST['price'],
+        "status" => $_POST['status']
+      );
+      $_SESSION['category_id'][] = $session_array;
+    }
+  }
+  ?>
+  <div class="container">
+  <div class="position-relative">
+  <div class="position-absolute me-5"></div>
+
+        <?php
+
+        $sql = "SELECT * FROM products WHERE category_id=4;";
+        $result = mysqli_query($conn, $sql);
+
+        while ($row = mysqli_fetch_array($result)) { ?>
+
+          
+            <form method="post" action="products.php?id=<?= $row['id'] ?>">
+              <div class="col-md-4">
+                <div class="section">
+                <li class="one_third">
+                  <article class="element">
+                    <figure>
+                      <img src="images/<?= $row['image'] ?>" style='height: 200px;'>
+                      <figcaption><a href="pay.php"><i class="fa fa-eye"></i></a></figcaption>
+                    </figure>
+                    <p><?= $row['name']; ?></p>
+                    <p><?= $row['description']; ?></p>
+                    <footer><a href="pay.php"><?= number_format($row['price'], 2); ?></p></a></footer>
+                    <input type="hidden" name="name" value="<?= $row['name'] ?>">
+                    <input type="hidden" name="price" value="<?= $row['price'] ?>">
+                    <input type="number" name="status" value="1" class="form-control">
+                    <input type="submit" name="add_to_card" class="btn btn-primary my-2" value="Choose">
+                  </article>
+                </li>
+              </div></div>
+            </form>
+          
+
+        <?php
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
+
+>>>>>>> adc8c2e331a27ad6c52dacab175d6e23831e9669
 
 <?php
 include_once 'footer.php';
